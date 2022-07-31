84f0b6058f30da54242ad3e01305d180b98dcc97
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2022-07-31 19:11:56 +0400

    Implemented log ids

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const fs = require("fs");
 const ipURL = "http://ipinfo.io/";
 
 function log(tolog) {
+    tolog = tolog.replace("DATE", new Date().toString())
+
+    // Get the count of how many logs have been made, by counting how many "NEW LOG" strings are in the log file
+    const logCount = fs.readFileSync("./log.log", "utf8").split("NEW LOG").length - 1;
+    const hexLogCount = parseInt(logCount).toString(16);
+
+    tolog = tolog.replace("LOGID", "0x" + (hexLogCount.toUpperCase()));
+
     fs.appendFile("./log.log", tolog + "\n", (err) => {
         if (err) {
             console.log(err);
@@ -10,39 +18,59 @@ function log(tolog) {
     });
 }
 
-const LOGTEMPLATE = `==========================================================
-New Log, ${new Date()}
+const LOGTEMPLATE =
+`
+                        _________
+                        |NEW LOG|
+=========================================================
+Date: DATE
+Log ID: LOGID
 
 DATA
 
-==========================================================
+(Log data from http://ipinfo.io)
+(NOTE: This data is not guaranteed to be accurate)
+=========================================================
 
 `
 
-http.get(ipURL, (response) => {
-    response.on("data", (data) => {
-        const requestData = JSON.parse(data);
+function getDataAndLog() {
+    log(`Attempting to get data, ${new Date()}`)
 
-        const details = 
-`IP: ${requestData.ip}
+    http.get(ipURL, (response) => {
+        response.on("data", (data) => {
+            const requestData = JSON.parse(data);
+            const details = 
+`
+IP: ${requestData.ip}
 Hostname: ${requestData.hostname}
 
 Country Code: ${requestData.country}
 City: ${requestData.city}
 Region: ${requestData.region}
+
 Latitude: ${requestData.loc.split(",")[0]}
 Longitude: ${requestData.loc.split(",")[1]}
 
 Postal Code: ${requestData.postal}
 Timezone: ${requestData.timezone}`;
 
-        log(LOGTEMPLATE.replace("DATA", details));
+            log(LOGTEMPLATE.replace("DATA", details));
 
-        console.log(requestData);
-    });
+            console.log(requestData);
+        });
 
-    }).on("error", (error) => {
-        log(LOGTEMPLATE.replace("DATA", `Error, couldn't log\nError Data: ${error.message}`));
-        console.log(error);
-    }
-);
+        }).on("error", (error) => {
+            log(LOGTEMPLATE.replace("DATA", `Error, couldn't log\nError Data: ${error.message}`));
+            console.log(error);
+        }
+    );
+}
+
+// getDataAndLog();
+
+function minToMs(min) {
+    return min * 60 * 1000;
+}
+
+setInterval(getDataAndLog, minToMs(0.1));
