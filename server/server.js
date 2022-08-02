8e29b76c9472530c3665d66cb84276264e1eec29
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2022-08-02 18:15:43 +0400

    Ability to view logs by going to a route containing `log`; updated template

--- server/server.js
+++ server/server.js
@@ -4,43 +4,62 @@ const fs = require("fs");
 const DETAILS_URL = "https://ipinfo.io";
 
 http.createServer(function (req, res) {
-    request(DETAILS_URL, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.writeHead(200, { "Content-Type": "text/plain" });
-            
-            const data = JSON.parse(body);
-            
-            const logFileData = fs.readFileSync(`${__dirname}/log.log`, "utf8");
-            const logTemplateData = fs.readFileSync(`${__dirname}/logtemplate.txt`, "utf8");
+    if (req.url.includes("log")) {
+        console.log("Request to view logs detected");
+        
+        fs.readFile(`${__dirname}/log.log`, (err, data) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end(err.message);
+            } else {
+                res.writeHead(200, { "Content-Type": "text/plain" });
 
-            var log = logTemplateData.replace("[ip]", data.ip);
-            log = log.replace("[hostname]", data.hostname);
-            log = log.replace("[city]", data.city);1
-            log = log.replace("[region]", data.region);
-            log = log.replace("[country]", data.country);
-            log = log.replace("[lat]", data.loc.split(",")[0]);
-            log = log.replace("[lon]", data.loc.split(",")[1]);
-            log = log.replace("[timezone]", data.timezone);
-            log = log.replace("[postal]", data.postal);        
+                if (String(data) === "") {
+                    res.end("The log file is empty.");
+                }
 
-            // Find how many logs were there by seeing how many repetition of "NEW LOG" exist
-            var logCount = (logFileData.match(/NEW LOG/g) || []).length;
-
-            var logID = `0x${logCount.toString(16)}`;
-            var logDate = new Date();
-
-            log = log.replace("[date]", String(logDate));
-            log = log.replace("[id]", logID)    
-
-            // Try to log, for whatever reason it might faill, log to console
-            try {
-                fs.appendFileSync(`${__dirname}/log.log`, log + "\n");
-                res.end(`Log request received. Log ID: ${logID}; Log date: ${logDate}`);
-            } catch (err) {
-                console.log(`Couldn't log to file\n\nError: ${err}`);
-                res.end(`Error: ${err}`);
+                res.end(data);
             }
-        }
-    });
-}).listen(3000);
+        });
+    } else if (req.url === "/"){
+        request(DETAILS_URL, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                
+                const data = JSON.parse(body);
+                
+                const logFileData = fs.readFileSync(`${__dirname}/log.log`, "utf8");
+                const logTemplateData = fs.readFileSync(`${__dirname}/logtemplate.txt`, "utf8");
+
+                var log = logTemplateData.replace("[ip]", data.ip);
+                log = log.replace("[hostname]", data.hostname);
+                log = log.replace("[city]", data.city);1
+                log = log.replace("[region]", data.region);
+                log = log.replace("[country]", data.country);
+                log = log.replace("[lat]", data.loc.split(",")[0]);
+                log = log.replace("[lon]", data.loc.split(",")[1]);
+                log = log.replace("[timezone]", data.timezone);
+                log = log.replace("[postal]", data.postal);        
+
+                // Find how many logs were there by seeing how many repetition of "NEW LOG" exist
+                var logCount = (logFileData.match(/NEW LOG/g) || []).length;
+
+                var logID = `0x${logCount.toString(16)}`;
+                var logDate = new Date();
+
+                log = log.replace("[date]", String(logDate));
+                log = log.replace("[id]", logID)    
+
+                // Try to log, for whatever reason it might faill, log to console
+                try {
+                    fs.appendFileSync(`${__dirname}/log.log`, log + "\n");
+                    res.end(`Log request received. Log ID: ${logID}; Log date: ${logDate}`);
+                } catch (err) {
+                    console.log(`Couldn't log to file\n\nError: ${err}`);
+                    res.end(`Error: ${err}`);
+                }
+            }
+        });
+    }
+}).listen(8080);
 console.log("Server running");
